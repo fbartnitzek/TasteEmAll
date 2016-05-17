33214fd681a7ce3904e6d310d946a5657de162a9
fbartnitzek <frank_bartnitzek@web.de>
2016-05-17 22:17:17 +0200

    fixed stubborn multidexing bug on android v4.3 device - and theoretically reduced app-size on the way

---
+++ app/src/main/java/com/fbartnitzek/tasteemall/CustomApplication.java
@@ -0,0 +1,30 @@
+package com.fbartnitzek.tasteemall;
+
+import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
+
+/**
+ * Copyright 2016.  Frank Bartnitzek
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+public class CustomApplication extends Application {
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        MultiDex.install(base); //enable multidex on v4.3 devices - part 3
+        super.attachBaseContext(base);
+    }
+}
