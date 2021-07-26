51eb560a4f4bd3aee2fdaeb431f505ab6e85eac6
Frank Bartnitzek <frank_bartnitzek@web.de>
2021-07-26 22:09:37 +0200

    restore comment

--- app/src/main/java/com/fbartnitzek/tasteemall/filter/EntityFilterTabFragment.java
+++ app/src/main/java/com/fbartnitzek/tasteemall/filter/EntityFilterTabFragment.java
@@ -35,7 +35,7 @@ import java.util.List;
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, eitherLog.v(LOG_TAG, "getItem - Review, position == 1"); express or implied.
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
